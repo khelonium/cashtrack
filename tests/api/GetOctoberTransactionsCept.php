c627bf28e130718413814b839675785ef908493c
Cosmin Dordea <cosmin.dordea@yopeso.com>
2013-11-26 23:26:40 +0200

    transaction test check modified

--- tests/api/GetOctoberTransactionsCept.php
+++ tests/api/GetOctoberTransactionsCept.php
@@ -4,4 +4,4 @@ $I->wantTo('GET transactions for October');
 $I->sendGET('transaction?month=2013-10-01');
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('{"id":"387","amount":"1453","fromAccount":"54","toAccount":"48","description":"387-description","date":"2013-10-10","reference":"","modified_timestamp":"2013-11-22 20:58:45"}');;
+$I->seeResponseContains('"id":"387","amount":"1453","fromAccount":"54","toAccount":"48","description":"387-description","date":"2013-10-10","reference":"');;
