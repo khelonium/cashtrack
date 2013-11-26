99c676f4474723e4ae19cf00fe3a6e9fb171e493
Cosmin Dordea <cosmin.dordea@yopeso.com>
2013-11-26 23:14:15 +0200

    api acceptance tests updated to match test data

--- tests/api/CashflowOctoberCept.php
+++ tests/api/CashflowOctoberCept.php
@@ -4,5 +4,4 @@ $I->wantTo('GET Cashflow list');
 $I->sendGET('cashflow',array('month' => '2013-10-01'));
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('{"name":"alocatie","accountId":"53","amount":"3700.00","type":"income","month":"2013-10-01"}');
-$I->seeResponseContains('{"name":"yopeso","accountId":"54","amount":"14100.00","type":"income","month":"2013-10-01"}');
+$I->seeResponseContains('{"name":"1-account","accountId":"1","amount":"1893.00","type":"expense","month":"2013-10-01"}');
