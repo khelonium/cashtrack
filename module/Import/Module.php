a84805ba5666d1dfdd21d211ea94e2f32d7ad342
Cosmin Dordea <cosmin.dordea@yopeso.com>
2015-03-14 23:06:45 +0800

    Database moved to psr autoloading

--- module/Import/Module.php
+++ module/Import/Module.php
@@ -10,9 +10,7 @@ class Module
     public function getAutoloaderConfig()
     {
         return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-            ),
+
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
@@ -21,24 +19,19 @@ class Module
         );
     }
 
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
 
     public function getServiceConfig()
     {
         return array(
 
             'factories' => array(
-               '\Import\BT\Matcher' => function ($sm) {
-                   return new Matcher($sm->get('\Database\Merchant\Repository')->all());
-               },
+                '\Import\BT\Matcher' => function ($sm) {
+                    return new Matcher($sm->get('\Database\Merchant\Repository')->all());
+                },
 
-            '\Import\BT\Parser' => function ($sm) {
-                   return new Parser($sm->get('\Import\BT\Matcher'));
-               },
+                '\Import\BT\Parser' => function ($sm) {
+                    return new Parser($sm->get('\Import\BT\Matcher'));
+                },
 
 
             ),
