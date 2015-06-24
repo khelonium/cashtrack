85b62ed5b77b8be970538054e20411808fc3b0a3
Cosmin Dordea <cosmin.dordea@yopeso.com>
2015-06-24 22:26:46 +0800

    display week total

--- public/js/views/cash/TransactionView.js
+++ public/js/views/cash/TransactionView.js
@@ -6,11 +6,11 @@ define([
     'views/Transaction',
     'collections/TransactionCollection',
     'd3'
-], function($, _, Backbone, AbstractTransaction, SingleTransactionView, TransactionCollection, d3){
+], function ($, _, Backbone, AbstractTransaction, SingleTransactionView, TransactionCollection, d3) {
 
     return AbstractTransaction.extend({
 
-        initialize:function() {
+        initialize: function () {
             this.collection = new TransactionCollection();
             this.collection.on('reset', this.render, this);
         },
@@ -19,13 +19,21 @@ define([
             console.log("Add all");
             this.$el.append('<tr><th colspan="3">Transactions</th></tr>');
 
-            var groupped = d3.nest().key(function(d){var day = new Date(d.attributes.date).getDate(); return (day - day%7)/7}).entries(this.collection.models);
+            var groupped = d3.nest().key(function (d) {
+                var day = new Date(d.attributes.date).getDate();
+                return (day - day % 7) / 7
+            }).entries(this.collection.models);
 
             groupped.forEach(this.addGroup, this);
         },
 
-        addGroup: function(group) {
-             this.$el.append('<tr class="active"><th colspan="3"> Week '+ (+group.key + 1) + '</th></tr>');
+        addGroup: function (group) {
+
+            var sum = d3.sum(group.values, function (d) {
+                return d.get('amount')
+            });
+            
+            this.$el.append('<tr class="active"><th colspan="1"> Week ' + (+group.key + 1) + '</th><th class="text-right">' + sum + '</th><th></th></tr>');
 
             group.values.forEach(this.addOne, this);
         },
@@ -44,7 +52,6 @@ define([
             this.$el.append(categoryView.el);
         }
     });
-
 
 
 });
