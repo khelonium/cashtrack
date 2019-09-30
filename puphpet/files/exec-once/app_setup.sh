7d2b3c17f2c968f1809659665a600b245828cd6f
Cosmin Dordea <cosmin.dordea@yopeso.com>
2019-09-30 17:40:21 +0800

    Replaces vagrant with docker
    - Vagrant completely removed
    - Docker has local image build
    - DB Setup missing

--- puphpet/files/exec-once/app_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /var/www
-php composer.phar self-update
-php composer.phar install
-cp build/vagrant/local.php config/autoload/local.orig.php
-cp build/vagrant/local.php config/autoload/local.php
-cp build/vagrant/test.php config/autoload/test.php
